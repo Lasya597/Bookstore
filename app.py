be38b3d05d8de608cba5404d43dd381df5e4d15f
Lasya597 <AVU22197578@student.ucb.ac.uk>
2025-01-07 12:11:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -201,5 +201,7 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    initialize_database()  # Ensure the database is ready before running the app
-    app.run(debug=True)
+    # initialize_database()  # Ensure the database is ready before running the app
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
